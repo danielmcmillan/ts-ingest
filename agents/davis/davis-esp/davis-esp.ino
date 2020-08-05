2d9d6d358ceb3a7f3dcb364f62aa3a1f66320d4b
Daniel McMillan <mcmillan.daniel3@gmail.com>
2020-08-05 20:45:53 +1000

    Initial pushing Davis data to SQS

--- agents/davis/davis-esp/davis-esp.ino
+++ agents/davis/davis-esp/davis-esp.ino
@@ -1,10 +1,11 @@
 #include "esp_wifi.h"
 #include "variables.h"
 #include "aws_request.h"
+#include "davis.h"
 
 using namespace aws_request;
 
-char body[200] = {0};
+char body[350] = {0};
 AWSRequest request(AWSRequestParameters{
     .method = "POST",
     .body = body,
@@ -19,6 +20,7 @@ unsigned int hello_count = 1;
 void setup()
 {
   Serial.begin(115200);
+  davis_setup();
   delay(500);
 }
 
@@ -26,8 +28,9 @@ void loop()
 {
   if (startWiFi(WIFI_SSID, WIFI_PASSWORD))
   {
-    Serial.printf("[General] Sending SNS message \"Hello number %u\".\n", hello_count);
-    sprintf(body, "Version=2010-03-31&Action=Publish&TopicArn=arn:aws:sns:ap-southeast-2:605337347611:davis-weather-ingest&Message=Hello number %u", hello_count);
+    sprintf(body, "Version=2010-03-31&Action=Publish&TopicArn=arn:aws:sns:ap-southeast-2:605337347611:davis-weather-ingest&Message=Hello number %u:", hello_count);
+    davis_go(body + strlen(body));
+    Serial.printf("[General] Sending SNS message %s.\n", body);
     hello_count += 1;
     AWSResponse response = request.send();
     Serial.printf("[General] Result: %d\n", response.status);
