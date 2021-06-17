098bdc961c375d95ae2fd0eab85ccf2225050f1c
Daniel McMillan <mcmillan@hotmail.com.au>
2021-06-17 15:25:35 +1000

    Remove the Davis ESP software

--- agents/davis/davis-esp/davis.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DAVIS_H__
-#define __DAVIS_H__
-
-void davis_setup();
-void davis_go(char *hex);
-
-#endif
--- agents/davis/davis-esp/variables.h
+++
@@ -1,5 +0,0 @@
-const char *AWS_ACCESS_KEY_ID = "";
-const char *AWS_SECRET_ACCESS_KEY = "";
-
-const char *WIFI_SSID = "";
-const char *WIFI_PASSWORD = "";
