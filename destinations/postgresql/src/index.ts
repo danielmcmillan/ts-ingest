47f1fc23d36cfc3bd730b7c7dfa0216d2c7e35a5
Daniel McMillan <mcmillan@hotmail.com.au>
2021-06-17 21:45:12 +1000

    Update postgres destination table schema

--- destinations/postgresql/src/index.ts
+++ destinations/postgresql/src/index.ts
@@ -1,34 +1,36 @@
-import { IDestination, IDataSample } from "../../../lib/dist";
+import { IDestination, IDataSample } from "@danielmcmillan/ts-ingest-lib";
 import pg from "pg";
 
 export class PostgresqlDestination implements IDestination {
-  constructor(public readonly options: {
-    table: string;
-    pg?: pg.ClientConfig;
-  }) { }
+  constructor(
+    public readonly options: {
+      table: string;
+      pg?: pg.ClientConfig;
+    }
+  ) {}
 
   async storeSamples(samples: IDataSample[]): Promise<void> {
     const ingestTime = new Date();
     const client = new pg.Client(this.options.pg);
     await client.connect();
-    const rows = samples.map(sample => [
+    const rows = samples.map((sample) => [
       new Date(sample.time),
       sample.source,
       sample.field,
       sample.value,
-      ingestTime
+      ingestTime,
     ]);
     await client.query(
-      `INSERT INTO ${this.options.table} (time, source, field, value, ingestTime)
+      `INSERT INTO ${this.options.table} (time, source, field, value, ingest_time)
         SELECT * FROM UNNEST ($1::TIMESTAMPTZ[], $2::TEXT[], $3::TEXT[], $4::DOUBLE PRECISION[], $5::TIMESTAMPTZ[])
         ON CONFLICT (time, source, field) DO UPDATE
-          SET value = excluded.value, ingestTime = excluded.ingestTime;`,
+          SET value = excluded.value, ingest_time = excluded.ingest_time;`,
       [
-        rows.map(row => row[0]),
-        rows.map(row => row[1]),
-        rows.map(row => row[2]),
-        rows.map(row => row[3]),
-        rows.map(row => row[4]),
+        rows.map((row) => row[0]),
+        rows.map((row) => row[1]),
+        rows.map((row) => row[2]),
+        rows.map((row) => row[3]),
+        rows.map((row) => row[4]),
       ]
     );
     await client.end();
