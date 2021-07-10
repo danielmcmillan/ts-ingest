3bc5e48539b2eb18038a8a413477b8020ba21409
Daniel McMillan <mcmillan@hotmail.com.au>
2021-07-10 18:00:16 +1000

    Use timestamp without timezone

--- README.md
+++ README.md
@@ -11,7 +11,12 @@ import { PostgresqlDestination } from "@danielmcmillan/ts-ingest-destination-pos
 const destinations = [
   new PostgresqlDestination({
     table: "table_name",
-    pg: { host: "localhost", user: "...", password: "...", database: "database_name" }
+    pg: {
+      host: "localhost",
+      user: "...",
+      password: "...",
+      database: "database_name",
+    },
   }),
 ];
 
@@ -29,4 +34,10 @@ STORAGE_URL="http://localhost:3001/" \
 SOURCE_NAME="..." \
 SITE_ID="..." \
 npx telemetry-agent
+```
+
+## davis-import agent
+
+```sh
+AWS_REGION=ap-southeast-2 DAVIS_SQS_URL="https://sqs.ap-southeast-2.amazonaws.com/605337347611/davis-db" STORAGE_URL="http://localhost:3001/" SOURCE_NAME="davis_1" /home/pi/.nvm/versions/node/v12.18.3/bin/node /home/pi/ingest/node_modules/.bin/davis-import
 ```
--- destinations/postgresql/src/index.ts
+++ destinations/postgresql/src/index.ts
@@ -10,11 +10,11 @@ export class PostgresqlDestination implements IDestination {
   ) {}
 
   async storeSamples(samples: IDataSample[]): Promise<void> {
-    const ingestTime = new Date();
+    const ingestTime = Date.now() / 1000;
     const client = new pg.Client(this.options.pg);
     await client.connect();
     const rows = samples.map((sample) => [
-      new Date(sample.time),
+      sample.time / 1000,
       sample.source,
       sample.field,
       sample.value,
@@ -22,7 +22,9 @@ export class PostgresqlDestination implements IDestination {
     ]);
     await client.query(
       `INSERT INTO ${this.options.table} (time, source, field, value, ingest_time)
-        SELECT * FROM UNNEST ($1::TIMESTAMPTZ[], $2::TEXT[], $3::TEXT[], $4::DOUBLE PRECISION[], $5::TIMESTAMPTZ[])
+        SELECT to_timestamp(ts) AT TIME ZONE 'UTC', source, field, value, to_timestamp(ingest_ts) AT TIME ZONE 'UTC'
+        FROM UNNEST ($1::DOUBLE PRECISION[], $2::TEXT[], $3::TEXT[], $4::DOUBLE PRECISION[], $5::DOUBLE PRECISION[])
+        AS u(ts, source, field, value, ingest_ts)
         ON CONFLICT (time, source, field) DO UPDATE
           SET value = excluded.value, ingest_time = excluded.ingest_time;`,
       [
